b21f5a3aafe7cfa7919ad62a7aa0e55e8b5f7daf
Nicola Felice <dev@dominiofelice.com>
2016-01-09 22:42:25 +0100

    Ask the user to confirm and backup the original configuration files
    Moved all the info inside the confirmation dialog:
    - show the detected GPU memory
    - show what files the script will change
    - show what parameters will change and what is the new value
    - inform the user of the existence of the timestamped backups
    
    Before changing the configuration files backup the originals.

--- UE3_Linux_Opti.sh
+++ UE3_Linux_Opti.sh
@@ -33,33 +33,56 @@ if [ "$MEM" == "" ]; then
 else let MEM=$MEM/1024
 fi
 
-
 let MEMORY=$MEM-128
+HALFMEMORY=$(($MEM / 10))
+gamesFound=`find ~/.local/share/ -type f -iname *.ini -exec grep -l ^MemoryMargin {} \;`
+timeStamp=`date +%Y-%m-%dT%H-%M-%S`
 
 if [ $language == "it_IT" ]; then
-    zenity --info \
-    --title="Memoria video" \
-    --text="La tua memoria video è di $MEM\MB, verrà impostata a $MEMORY\MB per evitare problemi."
+    msg="La memoria della GPU rilevata è $MEM\MB ed i file di configurazione trovati dei giochi che usano l'engine UE3 sono:\n\n"
+    msg+="$gamesFound\n\n"
+    msg+="I valori della configurazione modificati saranno:\n\n"
+    msg+="- PoolSize to $MEMORY\MB\n
+    msg+="- MemoryMargin to $HALFMEMORY\MB\n\n"
+    msg+="I file originali saranno conservati come \"(original file)_bck$timeStamp\"\n\n"
+    msg+="Continuare?"
+
+   zenity --question \
+    --title="Conferma ottimizzazione" \
+    --text="$msg"
 else
-    zenity --info \
-    --title="GPU Memory" \
-    --text="Your detected GPU memory is $MEM\MB, but it will be set to $MEMORY\MB to avoid issues."
+    msg="Your detected GPU memory is $MEM\MB and the configuration files found for games using the UE3 engine are:\n\n"
+    msg+="$gamesFound\n\n"
+    msg+="the configuration values changed will be:\n\n"
+    msg+="- PoolSize to $MEMORY\MB\n"
+    msg+="- MemoryMargin to $HALFMEMORY\MB\n\n"
+    msg+="The original files will be backed up as \"(original file)_bck$timeStamp\"\n\n"
+    msg+="Continue?"
+
+   zenity --question \
+    --title="Confirm optimization" \
+    --text="$msg"
 fi
+
+rc=$?
+if [ "${rc}" == "1" ]; then
+    echo "Program terminated by user."
+    exit 1
+fi
+
+# Must run again to avoid the problem with spaces in files
+# TODO: how to avoid the double run?
+find ~/.local/share/ -type f -iname *.ini -exec grep -l ^MemoryMargin {} \; | while read srcPath; do
+    cp "$srcPath" "$srcPath""_bck"$timeStamp
+done
 
 find ~/.local/share/ -type f -iname *.ini -exec sed -inr "s/^PoolSize=.*/PoolSize=$MEMORY/g" {} \;
+find ~/.local/share/ -type f -iname *.ini -exec sed -inr "s/^MemoryMargin=.*/MemoryMargin=$HALFMEMORY/g" {} \;
 
 if [ $language == "it_IT" ]; then
-    zenity --info --text "Tutti i tuoi giochi UE3 sono stati impostati a $MEMORY MB"
+    zenity --info --text "Ottimizzazione terminata con successo, GG"
 else
-    zenity --info --text "All your UE3 Games are optimized for $MEMORY\MB GPU."
-fi
-HALFMEMORY=$(($MEM / 10))
-
-find ~/.local/share/ -type f -iname *.ini -exec sed -inr "s/^MemoryMargin=.*/MemoryMargin=$HALFMEMORY/g" {} \;
-if [ $language == "it" ]; then
-    zenity --info --text "MemoryMargin impostato a $HALFMEMORY MB"
-else
-    zenity --info --text "MemoryMargin has been set to $HALFMEMORY MB"
+    zenity --info --text "Optimization successfully terminated, GG"
 fi
 
 exit
